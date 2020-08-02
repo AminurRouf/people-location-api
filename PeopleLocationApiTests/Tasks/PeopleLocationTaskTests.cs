af9e2d1d6530ff860e0f224aeb8a502c2655075d
CAMPUS\nar25 <aminur.rouf@newcastle.ac.uk>
2020-08-02 22:43:52 +0100

    Added a few necessary  comments. Fixed issue with logic in GetPeopleLivingInOrCoordinatesWithInFiftyMiles which was not returning a distinct list of people. Test GetPeopleLivingInOrCoordinatesWithInFiftyMiles to ensure it returns as distinct list.

--- PeopleLocationApiTests/Tasks/PeopleLocationTaskTests.cs
+++ PeopleLocationApiTests/Tasks/PeopleLocationTaskTests.cs
@@ -49,5 +49,28 @@ namespace PeopleLocationApiTests.Tasks
             result.ShouldNotBeNull();
             result.Count().ShouldBe(people.Count);
         }
+
+        [Fact]
+        public async void UsersEitherLivingInOrWithinFiftyMilesLondonShouldReturnDistinctPeople()
+        {
+            // Arrange
+            const double miles = 50;
+            var users = PeopleTestData.GetUsers();
+            var people = PeopleTestData.GetPeople();
+            var expectedCount = 1;// Cause first person in users and people are the same
+            var mock = new Mock<IBpdtsTestAppService>();
+            mock.Setup(x => x.GetUsers()).ReturnsAsync(users).Verifiable();
+            mock.Setup(x => x.GetPeopleLivingIn(LondonCityConstants.Name)).ReturnsAsync(people).Verifiable();
+            var sut = new PeopleLocationTask(mock.Object);
+
+            //Act
+            var result = await sut.GetPeopleLivingInOrCoordinatesWithInFiftyMiles(LondonCityConstants.Name, miles);
+
+            //Assert
+            mock.Verify(x => x.GetUsers(), Times.Once);
+            mock.Verify(x => x.GetPeopleLivingIn(LondonCityConstants.Name), Times.Once);
+            result.ShouldNotBeNull();
+            result.Count().ShouldBe(expectedCount);
+        }
     }
 }
